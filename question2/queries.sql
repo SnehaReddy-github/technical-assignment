46ab212b53e72884d0a230a5d2a799e81d49b4a3
SnehaReddy-github <173171017+SnehaReddy-github@users.noreply.github.com>
2026-08-29 07:32:09 +0530

    Update queries.sql

--- question2/queries.sql
+++ question2/queries.sql
@@ -4,26 +4,26 @@
 -- in the taxonomy table.
 -- ============================================================
 
--- SELECT COUNT(*) AS acacia_count
-    FROM taxonomy
-    WHERE species LIKE 'Acacia%';
+SELECT COUNT(*) AS acacia_count
+FROM taxonomy
+WHERE species LIKE 'Acacia%';
 
 -- ============================================================
 -- Question 2B
 -- Determine which type of wheat has the longest DNA sequence.
 -- ============================================================
 
--- SELECT
-    t.species,
-    MAX(r.length) AS longest_dna_length
-    FROM taxonomy AS t
-    JOIN rfamseq AS r
-        ON t.ncbi_id = r.ncbi_id
-    WHERE t.ncbi_id IN (4565, 4567)
-    AND r.mol_type IN ('DNA', 'genomic DNA')
-    GROUP BY t.species
-    ORDER BY longest_dna_length DESC
-    LIMIT 1;
+SELECT
+t.species,
+MAX(r.length) AS longest_dna_length
+FROM taxonomy AS t
+JOIN rfamseq AS r
+ON t.ncbi_id = r.ncbi_id
+WHERE t.ncbi_id IN (4565, 4567)
+AND r.mol_type IN ('DNA', 'genomic DNA')
+GROUP BY t.species
+ORDER BY longest_dna_length DESC
+LIMIT 1;
 
 -- ============================================================
 -- Question 2C
@@ -35,27 +35,27 @@
 -- Page 9 = OFFSET 120, LIMIT 15.
 -- ============================================================
 
--- SELECT
-    f.rfam_id AS family_name,
-    f.rfam_acc AS family_accession,
-        MAX(r.length) AS maximum_dna_sequence_length
-    FROM family AS f
-    JOIN full_region AS fr
-        ON f.rfam_acc = fr.rfam_acc
-    JOIN rfamseq AS r
-        ON fr.rfamseq_acc = r.rfamseq_acc
-    WHERE r.mol_type IN (
-        'DNA',
-        'genomic DNA',
-        'ss-DNA',
-        'other DNA',
-        'unassigned DNA'
+SELECT
+f.rfam_id AS family_name,
+f.rfam_acc AS family_accession,
+MAX(r.length) AS maximum_dna_sequence_length
+FROM family AS f
+JOIN full_region AS fr
+ON f.rfam_acc = fr.rfam_acc
+JOIN rfamseq AS r
+ON fr.rfamseq_acc = r.rfamseq_acc
+WHERE r.mol_type IN (
+    'DNA',
+    'genomic DNA',
+    'ss-DNA',
+    'other DNA',
+    'unassigned DNA'
     )
-    GROUP BY
-        f.rfam_id,
-        f.rfam_acc
-    HAVING MAX(r.length) > 1000000
-    ORDER BY
-        maximum_dna_sequence_length DESC,
-        family_accession ASC
-    LIMIT 15 OFFSET 120;
+GROUP BY
+f.rfam_id,
+f.rfam_acc
+HAVING MAX(r.length) > 1000000
+ORDER BY
+maximum_dna_sequence_length DESC,
+family_accession ASC
+LIMIT 15 OFFSET 120;
